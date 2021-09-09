267a2687ef4ea9864a3735a949ebc91799206093
Burak Aksu <sabburak26@gmail.com>
2021-09-09 16:57:51 -0400

    testing with people routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentsRoutes');
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./peopleRoutes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
